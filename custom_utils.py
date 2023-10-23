8abc8c1d62a7f1310fa60f505d6682c0b8790278
ariyaltharun <ariyaltharun@gmail.com>
2023-10-23 20:01:42 +0000

    Fixed bug
    * Was creating empty train.csv file [ FIXED ]
    * The path of the image wasn't correct [ FIXED ]

--- custom_utils.py
+++ custom_utils.py
@@ -6,27 +6,25 @@ def create_csv(root: Path, pd: pd = pd):
     Output: 
     - Creates two file `train.csv` and `test.csv`
     """
-
+    train = {
+        'img_path': [],
+        'labels': []
+    }
+    test = {
+        'img_path': [],
+        'labels': []
+    }
     for status in root.iterdir():
-        train = {
-            'img_path': [],
-            'labels': []
-        }
-        test = {
-            'img_path': [],
-            'labels': []
-        }
         class_names = root / status
         for class_name in class_names.iterdir():
             images = root / class_name
-            img_path = f"Dataset/{class_name}/"
             for image in images.iterdir():
                 if status == 'train':
-                    train['img_path'].append(img_path+str(image))
-                    train['labels'].append(class_name)
+                    train['img_path'].append("Dataset/"+str(image))
+                    train['labels'].append(str(class_name)[6:])
                 if status == 'test':
-                    test['img_path'].append(img_path+str(image))
-                    test['labels'].append(class_name)
+                    test['img_path'].append("Dataset/"+str(image))
+                    test['labels'].append(str(class_name)[5:])
 
     pd.DataFrame(train).to_csv("train.csv", index=False)
     pd.DataFrame(test).to_csv("test.csv", index=False)
